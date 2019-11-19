366b9353221b1a4db77743e865f3135fb5e13982
Swizel <54148148+swizel012@users.noreply.github.com>
2019-11-19 11:02:54 +0530

    calculator final

--- calci.js
+++ calci.js
@@ -1,14 +1,23 @@
+
 function insert(num){
 form.textview.value +=num;
 }
 function equal(){
-    form.textview.value=eval(form.textview.value)
+    form.oldtext.value= form.textview.value;
+    form.textview.value=eval(form.textview.value);
+    
 }
 
 function clearAll(){
-    form.textview.value=" "
+    form.oldtext.value=" ";
+    form.textview.value=" ";
+  
 }
 function back(){
     var exp=form.textview.value;
     form.textview.value=exp.substring(0,exp.length-1);
 }
+
+
+
+
